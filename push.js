e7a13e7951d4d4776065d10cf99e138052dbc557
Deniz Burduroglu <denizburduroglu@gmail.com>
2021-07-15 12:07:52 -0400

    Working push notification

--- push.js
+++ push.js
@@ -9,7 +9,8 @@ const swaggerDocument = require('./swagger.json');
 
 // Push notification details
 const webpush = require('web-push');
-const vapidKeys = webpush.generateVAPIDKeys();
+const vapidPublicKey = 'BPFJMQ-YaJTkX99MBgc9tW9ov06LQ1-KcBzeR_5kkYPKZ-XY1Syy_7AT8awnL8GECYdslzfylMF2lUJpk-jHq50';
+const vapidPrivateKey = 'k7gCqKEHSDWoQ26wZXqr8nmq_vMOfghAWvyKne8Rwoo';
 
 // Hanlde push notifications from app to send to service worker
 app.use(express.json());
@@ -28,18 +29,13 @@ app.post('/notification', (req, res) => {
     res.set('Content-Type', 'application/json');
     webpush.setVapidDetails(
         'mailto:daburduroglu@gmail.com',
-        vapidKeys.publicKey,
-        vapidKeys.privateKey
+        vapidPublicKey,
+        vapidPrivateKey
     );
     let payload = JSON.stringify({
         "notification":{
             "title":"This is my notification",
-            "body":"Your timer is up",
-            "icon":"https://www.shareicon.net/data/256x256/2015/10/02/110808_blog_512x512.png",
-            "vibrate":[100,50,100],
-            "data":{
-                "url":"https://the-boss-of-sauce.herokuapp.com"
-            }
+            "body":"Your timer is up"
         }
     });
     console.log("Send Notification: ", sub);
